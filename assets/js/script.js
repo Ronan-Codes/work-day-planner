3643f88817020323206ada196be6c0ddf9d8c764
Ronan <galv.ronan@gmail.com>
2021-03-21 00:27:20 -0400

    Added time auditing for background colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var tasks = {}
 var loadTasks = function() {
     tasks = JSON.parse(localStorage.getItem("tasks"));
 
-    // if nothing in localStorage, create a new object to track all task time arrays
+    // If nothing in localStorage, create a new object to track all task time arrays
     if (!tasks) {
         tasks = {
             9: [],
@@ -21,13 +21,9 @@ var loadTasks = function() {
     // loop over object properties
     $.each(tasks, function(time, arr) {
         arr.forEach(function(task) {
-            var taskId = task.taskId;
             var text = task.text;
 
-            console.log(taskId);
-            console.log(time)
-            console.log(text)
-
+            // Load tasks for each time block
             $("textarea[data-id='" + time + "']").val(text);
         })
     })
@@ -37,23 +33,49 @@ var loadTasks = function() {
 // Current Date
 var currentDay = moment().format('dddd, MMMM Do')
 $("#currentDay").text(currentDay)
+// Current Time
+var currentTime = moment().hours()
+var currentTimeParsed = parseInt(currentTime)
 
-// when you click on a save button, prevent reset? and save task to Array then localStorage
+
+// Audit Tasks for color coding
+var auditTask = function() {
+    $("textarea").each(function() {
+        var hourTest = parseInt($(this).attr("id"));
+
+        // if (hour > currentTime)
+        if (currentTimeParsed > hourTest) {
+            $(this).addClass("past");
+        }
+        else if (currentTimeParsed < hourTest) {
+            $(this).addClass("future");
+        }
+        else {
+            $(this).addClass("present");
+        }
+    })
+
+}
+
+
+// Save Task Handler
 $(".input-group").on("click", "button", function() {
     event.preventDefault();
 
-    // get data-id of button
+    // Get data-id of button
     var taskId = $(this).attr("data-id");
-    // get value of corresponding textarea
+    // Get task value of corresponding textarea
     var taskContent = $("textarea[data-id='"+taskId+"']").val()
 
+    // Update tasks array
     tasks[taskId].push({
         text: taskContent,
         taskId: taskId
     })
 
+    // Update localStorage
     localStorage.setItem("tasks", JSON.stringify(tasks))
-
 })
 
+auditTask();
 loadTasks();
